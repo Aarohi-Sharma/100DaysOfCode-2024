b712759f5794393b21ee5d6a4eb42cbdf5beb320
Brijanshi <142512862+Brijanshi@users.noreply.github.com>
2024-09-25 01:27:35 +0530

    Q1.cpp

---
+++ DSA/Brijanshi/Day_66/Q1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stack>
+#include <map>
+using namespace std;
+
+class FreqStack {
+public:
+    map<int, int> freq;
+    map<int, stack<int>> st;
+    int maxFreq = 0;
+
+    FreqStack() {
+    }
+    
+    void push(int val) {
+        freq[val]++;
+        st[freq[val]].push(val);
+        maxFreq = max(maxFreq, freq[val]);
+    }
+    
+    int pop() {
+        if(st[maxFreq].empty()) {
+            maxFreq--;
+        }
+        int x = st[maxFreq].top();
+        freq[x]--;
+        st[maxFreq].pop();
+        return x;
+    }
+};
